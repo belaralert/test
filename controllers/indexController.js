0fac5031b1c7d9385e8c326d7ef086a2c8826bf4
Araceli Belen Mamani Catacora <yeolsoo73@gmail.com>
2021-07-28 22:21:48 -0300

    probando json

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,11 @@
-const products= require('../data/products_db');
+const sections = require('../data/sections_db');
+const products = require('../data/products_db');
+
+
 module.exports = {
     index : (req, res)=>{
+        /* console.log(sections) */
+
         return res.render('index',{
             title : "Oh Shots",
             products,
